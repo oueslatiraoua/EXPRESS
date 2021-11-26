7dd2fc04e8d702f34656886317ac65efbb225f03
raoua <oueslatiraoua@gmail.com>
2021-11-26 12:25:12 +0100

    first commit

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const myapp = express();
-console.log(myapp);
-myapp.use(express.static("public"));
-function currenttime() {
-  return new Date().toString();
-}
-
-myapp.get(
-  "/now",
-  (req, res, next) => {
-    req.time = currenttime();
+myapp.get("/style.css", (req, res) => {
+  res.sendFile(__dirname + "/style.css");
+});
+myapp.use((req, res, next) => {
+  let date = new Date();
+  if (
+    date.getDay() > 0 &&
+    date.getDay() < 6 &&
+    date.getHours() > 9 &&
+    date.getHours() < 17
+  ) {
     next();
-  },
-  (req, res) => res.json({ time: req.time })
-);
+  } else {
+    return res.sendFile(__dirname + "/notfound.html");
+  }
+});
+myapp.use(express.static("public"));
 
 myapp.listen(5000, (err) =>
   err ? console.log(err) : console.log("is running")
